efb9679b16d9710924c9dfee1a262a23ef91f4b3
_leodev <84308529+leonardof02@users.noreply.github.com>
2023-12-11 17:20:39 -0500

    feat: Add a TabListView for navigate between screens

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,24 +7,32 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            "Viajes UCI / Pinar del Rio",
-            style: TextStyle(fontWeight: FontWeight.w400, color: Colors.white),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+          appBar: AppBar(
+            bottom: const TabBar(tabs: [
+              Tab(text: "Personas Agregadas"),
+              Tab(text: "Lista de reservas")
+            ]),
+            title: const Text(
+              "Viajes UCI / Pinar del Rio",
+              style:
+                  TextStyle(fontWeight: FontWeight.w400, color: Colors.white),
+            ),
+            backgroundColor: Colors.blueGrey[800],
           ),
-          backgroundColor: Colors.blueGrey[800],
-        ),
-        floatingActionButton: FloatingActionButton.extended(
-            onPressed: () {
-              showModalBottomSheet(
-                  context: context,
-                  builder: (context) {
-                    return AddPersonForm();
-                  });
-            },
-            label: const Text("Agregar Persona"),
-            icon: const Icon(Icons.person_add_alt_1_sharp)),
-        body: const PersonList());
+          floatingActionButton: FloatingActionButton.extended(
+              onPressed: () {
+                showModalBottomSheet(
+                    context: context,
+                    builder: (context) {
+                      return AddPersonForm();
+                    });
+              },
+              label: const Text("Agregar Persona"),
+              icon: const Icon(Icons.person_add_alt_1_sharp)),
+          body: const TabBarView(children: [const PersonList(), Placeholder()])),
+    );
   }
 }
