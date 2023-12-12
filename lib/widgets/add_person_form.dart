bb8609b9f38e4ea2a1ebf9a967d8f0613fcfafe0
_leodev <84308529+leonardof02@users.noreply.github.com>
2023-12-12 00:30:45 -0500

    feat: Form Validations and add Person Feature

--- lib/widgets/add_person_form.dart
+++ lib/widgets/add_person_form.dart
@@ -1,21 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:viajes_uci_pr/models/person.dart';
+import 'package:viajes_uci_pr/riverpod/person_notifier.dart';
+import '../constants/destinations.dart';
 
-class AddPersonForm extends StatelessWidget {
+class AddPersonForm extends ConsumerWidget {
   final idController = TextEditingController(text: "");
   final nameController = TextEditingController(text: "");
   final destinationController = TextEditingController(text: "Pinar del Rio");
-  final List<String> destinationList = [
-    'Pinar del Rio',
-    'Puerta de Golpe',
-    'Consolacion',
-    'Herradura',
-    'Los Palacios',
-    'Paso Real',
-    'Taco Taco',
-    'San Cristobal',
-    'Candelaria',
-    'Las Terrazas'
-  ];
+  final formKey = GlobalKey<FormState>();
 
   AddPersonForm({super.key});
 
@@ -23,62 +16,94 @@ class AddPersonForm extends StatelessWidget {
     destinationController.text = newValue ?? destinationController.text;
   }
 
+  String? validateFullName(String? value) {
+    RegExp nameRegex = RegExp(r"^[a-zA-ZñÑ]+(\s[a-zA-ZñÑ]+)+$");
+    if (value != null && !nameRegex.hasMatch(value))
+      return "Por favor escribe nombre y apellidos";
+    return null;
+  }
+
+  String? validateId(String? value) {
+    RegExp idRegex = RegExp(r"^\d{11}$");
+    if (value != null && !idRegex.hasMatch(value))
+      return "Formato de Carnet de ID no valudo";
+    return null;
+  }
+
+  void addNewPerson(WidgetRef ref) {
+    ref.read(personProvider.notifier).addPerson(Person(
+        id: idController.text,
+        fullName: nameController.text,
+        destination: destinationController.text,
+        isReserved: false));
+  }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Container(
       padding: const EdgeInsets.all(20),
       child: Form(
+          key: formKey,
           child: Column(
-        children: [
-          TextFormField(
-            controller: idController,
-            decoration: const InputDecoration(
-              hintText: "Escriba su numero de CI",
-              border: OutlineInputBorder(),
-              label: Text("CI"),
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          TextFormField(
-            controller: nameController,
-            decoration: const InputDecoration(
-              hintText: "Escriba su nombre y apellido",
-              border: OutlineInputBorder(),
-              label: Text("Nombre y Apellido"),
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          DropdownButtonFormField<String>(
-              value: destinationController.text,
-              decoration: const InputDecoration(
-                contentPadding: EdgeInsets.all(10),
-                labelText: 'Selecciona un valor',
+            children: [
+              TextFormField(
+                controller: idController,
+                validator: validateId,
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: const InputDecoration(
+                  hintText: "Escriba su numero de CI",
+                  border: OutlineInputBorder(),
+                  label: Text("CI"),
+                ),
               ),
-              items: destinationList
-                  .map(
-                      (e) => DropdownMenuItem<String>(value: e, child: Text(e)))
-                  .toList(),
-              onChanged: handleChange),
-          const SizedBox(
-            height: 20,
-          ),
-          SizedBox(
-              width: double.infinity,
-              child: FilledButton(
-                  onPressed: () {
-                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                      content: Text("Persona Agregada con exito"),
-                      backgroundColor: Colors.green,
-                    ));
-                    Navigator.of(context).pop();
-                  },
-                  child: const Text("Agregar Persona")))
-        ],
-      )),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: nameController,
+                validator: validateFullName,
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: const InputDecoration(
+                  hintText: "Escriba su nombre y apellido",
+                  border: OutlineInputBorder(),
+                  label: Text("Nombre y Apellido"),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              DropdownButtonFormField<String>(
+                  value: destinationController.text,
+                  decoration: const InputDecoration(
+                    contentPadding: EdgeInsets.all(10),
+                    border: UnderlineInputBorder(),
+                    floatingLabelBehavior: FloatingLabelBehavior.auto,
+                    prefixIcon: Icon(Icons.place_outlined),
+                    labelText: 'Selecciona el puente donde te quedas',
+                  ),
+                  items: destinationList
+                      .map((e) =>
+                          DropdownMenuItem<String>(value: e, child: Text(e)))
+                      .toList(),
+                  onChanged: handleChange),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                      onPressed: () {
+                        if (formKey.currentState!.validate()) addNewPerson(ref);
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(const SnackBar(
+                          content: Text("Persona Agregada con exito"),
+                          backgroundColor: Colors.green,
+                        ));
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text("Agregar Persona")))
+            ],
+          )),
     );
   }
 }
